31d6f53c190369bd9afed21c93280c57a7fc5096
Breno Baptista <brenobaptista@protonmail.com>
2021-10-05 00:35:43 -0300

    Added Congratulations container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Congratulations from "./containers/Congratulations";
 import Vehicle from "./containers/Vehicle";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Route exact path="/" component={Vehicle} />
+      <Route path="/congratulations" component={Congratulations} />
     </BrowserRouter>
   );
 };
