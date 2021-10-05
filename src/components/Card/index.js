386ffe5ed467a41a5b3b076323c188433614f414
Breno Baptista <brenobaptista@protonmail.com>
2021-10-05 00:37:18 -0300

    Added Card component

---
+++ src/components/Card/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./index.css";
+
+const Card = ({
+  photo,
+  info,
+  mileage,
+  lender,
+  annualPercentage,
+  monthlyPayment,
+  timeRemaining,
+}) => {
+  return (
+    <div className="card">
+      <img src={photo} alt="Vehicle" className="vehicle-image" />
+      <div className="card-info">
+        <div className="vehicle-info">
+          <div className="important">{info}</div>
+          <div className="vehicle-mileage">
+            with an estimated <span className="important">{mileage}</span> miles
+          </div>
+        </div>
+        <div className="loan-info">
+          <div>Lender</div>
+          <div className="important">{lender}</div>
+        </div>
+        <hr className="hr" />
+        <div className="loan-info">
+          <div>APR</div>
+          <div className="important">{annualPercentage}</div>
+        </div>
+        <hr className="hr" />
+        <div className="loan-info">
+          <div>Monthly payment</div>
+          <div className="important">{monthlyPayment}</div>
+        </div>
+        <hr className="hr" />
+        <div className="loan-info">
+          <div>Time remaining</div>
+          <div className="important">{timeRemaining}</div>
+        </div>
+        <button type="button" className="btn">
+          Select loan
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
