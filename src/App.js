b165074f00343f725717d67f7eedc1529e0fd72a
Breno Baptista <brenobaptista@protonmail.com>
2021-10-05 14:54:11 -0300

    Included Offers in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-
 import Congratulations from "./containers/Congratulations";
-import Vehicle from "./containers/Vehicle";
+import Offers from "./containers/Offers";
+import Vehicles from "./containers/Vehicles";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Vehicle} />
+      <Route exact path="/" component={Vehicles} />
+      <Route path="/offers/:vehicleId" component={Offers} />
       <Route path="/congratulations" component={Congratulations} />
     </BrowserRouter>
   );
